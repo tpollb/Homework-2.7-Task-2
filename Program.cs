f2a23d7b5110e40bc11593f5acfb578ee6e63eda
Сергей Усков <q3mydoom@hotmail.com>
2021-10-25 17:12:09 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Homework_2._7_Task_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            //переменные
+            string Name = "Сергей";
+            string Surname = "Усков";
+            string Secondname = "Евгеньевич";
+
+            int ProgrammingGrade = 1;
+            int MathsGrade = 2;
+            int PhysicsGrade = 3;
+
+            int SumGrade = ProgrammingGrade + MathsGrade + PhysicsGrade;    //сумма баллов (см коммент ниже)
+            int[] array = {ProgrammingGrade, MathsGrade, PhysicsGrade};     //массив для среднего значения. изначально надо было сделать струкутуру массивом, но в задаче 1 не требовалось
+            int AvgGrade = SumGrade / array.Length;                         //прошу прощения за кривоту
+
+            Console.WriteLine(" Summing Grade: {0}\n Avg Grade: {1}\n", SumGrade, AvgGrade);
+
+            Console.ReadKey();
+
+        }
+    }
+}
